c96db0926a4ee8aec4cc4282bf5257ff14f4b675
Kerry Zhu <rm.kzhu@gmail.com>
2014-01-24 20:21:16 -0500

    Add steam

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Steamtest::Application.config.secret_key_base = '0356a43e1581ed63c4ff44063723625d7a5894c8b103d1dd3750137cba680319685cbf90f0c3e0d44c97aa6377a1ac24851a12f7891987ea9b61fa398baee3fd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Steamtest::Application.config.session_store :cookie_store, key: '_steamtest_session'
